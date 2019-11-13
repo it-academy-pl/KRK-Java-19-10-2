70cf4dbe3221bc1840052a918e39d83742ed5346
olegmiercoles <oleg.miercoles@dot.com>
2019-11-13 21:16:01 +0100

    Remove ING Bank

--- src/pl/itacademy/lesson11/Bank.java
+++ src/pl/itacademy/lesson11/Bank.java
@@ -1,6 +1,41 @@
 package pl.itacademy.lesson11;
 
-public abstract class Bank {
-    protected abstract String getName();
-    protected abstract Address getAddress();
+public class Bank {
+
+    private String name;
+
+    private Address address;
+
+    private Account[] accounts;
+
+    private int lastAccountIndex;
+
+    public Bank(String name, Address address) {
+        this.name = name;
+        this.address = address;
+        accounts = new Account[100];
+        lastAccountIndex = 0;
+    }
+
+    protected String getName() {
+        return name;
+    }
+
+    protected Address getAddress() {
+        return address;
+    }
+
+    public void addAccount(Account account) {
+        accounts[lastAccountIndex] = account;
+        lastAccountIndex++;
+    }
+
+    public Account getAccountByNumber(int accountNumber) {
+        for (Account account : accounts) {
+            if (account.getAccountNumber() == accountNumber) {
+                return account;
+            }
+        }
+        return null;
+    }
 }
